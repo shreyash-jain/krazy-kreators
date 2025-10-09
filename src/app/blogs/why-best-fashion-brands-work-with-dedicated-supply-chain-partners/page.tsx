4fbd974f82f575a73a30798a0f404c44bfe4ddef
Shreyash Jain <shreyashjain@Shreyashs-MacBook-Air.local>
2025-10-09 14:55:02 +0530

    added fixes

--- src/app/blogs/why-best-fashion-brands-work-with-dedicated-supply-chain-partners/page.tsx
+++ src/app/blogs/why-best-fashion-brands-work-with-dedicated-supply-chain-partners/page.tsx
@@ -5,7 +5,7 @@ import { headers } from 'next/headers';
 export const dynamic = 'force-dynamic';
 
 export default async function SupplyChainBlogPage() {
-  const headersList = headers();
+  const headersList = await headers();
   const host = headersList.get('x-forwarded-host') ?? headersList.get('host');
   const proto = headersList.get('x-forwarded-proto') ?? 'https';
   const baseUrl = host ? `${proto}://${host}` : undefined;
