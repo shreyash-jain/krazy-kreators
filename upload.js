b90f2f5cad7156387ff581891bc3cc49e8870311
Aadi Jain <181472107+Aadi-j08@users.noreply.github.com>
2026-04-25 13:56:21 +0530

    addedblog 8

--- upload.js
+++ upload.js
@@ -1,23 +1,25 @@
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
-  cloud_name: 'dn9snfizy', 
-  api_key: '644429988616382', 
-  api_secret: 'd11XIyI5e12AJ--FnAeQRPh6j68' 
+cloudinary.config({
+    cloud_name: 'dn9snfizy',
+    api_key: '644429988616382',
+    api_secret: 'd11XIyI5e12AJ--FnAeQRPh6j68'
 });
 
 const uploadImages = async () => {
     const images = [
         { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\essential_trimmings_banner.png", public_id: "blog/essential_trimmings_banner" },
         { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\premium_zipper_hardware.png", public_id: "blog/premium_zipper_hardware" },
-        { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\custom_drawstring_aglet.png", public_id: "blog/custom_drawstring_aglet" }
+        { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\custom_drawstring_aglet.png", public_id: "blog/custom_drawstring_aglet" },
+        { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\eu_sustainability_banner.png", public_id: "blog/eu_sustainability_banner" },
+        { path: "G:\\gitt repoo\\krazy-kreators\\public\\blog\\eu_sustainability_fabric.png", public_id: "blog/eu_sustainability_fabric" }
     ];
 
     for (const img of images) {
         try {
             const res = await cloudinary.uploader.upload(img.path, { public_id: img.public_id });
             console.log(`UPLOAD_SUCCESS|${img.public_id}`);
-        } catch(e) {
+        } catch (e) {
             console.error(`UPLOAD_ERROR|${img.public_id}|`, e);
             process.exit(1);
         }
